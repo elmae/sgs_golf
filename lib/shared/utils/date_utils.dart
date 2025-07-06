265395fe1c9056cc2d0ef2b89b62c7a7982a804c
elmae <13579755+elmae@users.noreply.github.com>
2025-07-06 11:04:06 -0400

    fix(lint): use single quotes in date_utils.dart

--- lib/shared/utils/date_utils.dart
+++ lib/shared/utils/date_utils.dart
@@ -1,8 +1,8 @@
 class DateUtilsShared {
   /// Devuelve la fecha en formato dd/MM/yyyy
   static String formatDate(DateTime date) {
-    return "${date.day.toString().padLeft(2, '0')}/"
-        "${date.month.toString().padLeft(2, '0')}/"
-        "${date.year}";
+    return '${date.day.toString().padLeft(2, '0')}/'
+           '${date.month.toString().padLeft(2, '0')}/'
+           '${date.year}';
   }
 }
